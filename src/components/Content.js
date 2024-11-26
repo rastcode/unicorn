f1f1b02536462c7fe4b82fedd0270c63675fe432
ali <nematiali1377@gmail.com>
2024-11-26 15:57:02 -0400

    v1

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className='w-full bg-slate-50 py-5 px-2 flex items-center justify-center rounded-lg shadow-xl'>عشق نازنینم خیلی دوست دارم</div>
+  )
+}
+
+export default Content
